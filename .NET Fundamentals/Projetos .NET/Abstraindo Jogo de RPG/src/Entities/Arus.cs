fb355af88cb1ddfb4321b8292cc6a78e0b08be74
Flimars <flavio.lima@aluno.riogrande.ifrs.edu.br>
2022-06-10 11:58:41 -0300

    Iniciando Novo Projeto em POO.

---
+++ .NET Fundamentals/Projetos .NET/Abstraindo Jogo de RPG/src/Entities/Arus.cs
@@ -0,0 +1,16 @@
+namespace Abstraindo_Jogo_de_RPG.src.Entities
+{
+    public class Arus
+    {
+        string Name;
+        int Level;
+        string Type;
+        public Arus(string Name, int Level, string Type)
+        {
+         this.Name = Name;
+         this.Level = Level; 
+         this.Type = Type; 
+            
+        }
+    }
+}
