aad8c120124d5c83ea71f00d50cf909325917873
Flimars <flavio.lima@aluno.riogrande.ifrs.edu.br>
2022-06-27 22:12:08 -0300

    Implementando Stopwatch em C# 270622

--- .NET Fundamentals/Projetos .NET/Project Balta.io/Stopwatch/Program.cs
+++ .NET Fundamentals/Projetos .NET/Project Balta.io/Stopwatch/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Menu();
+    }
+
+    static void Menu()
+    {
+      Console.Clear();
+      System.Console.WriteLine("S = Segundo -> 10s = 10 segundos");
+      System.Console.WriteLine("M = Minuto -> 1m = 1 minuto");
+      System.Console.WriteLine("X = Sair");
+      System.Console.WriteLine("Informe quanto tempo quer contar:");
+
+      string data = Console.ReadLine().ToLower();
+      char type = char.Parse(data.Substring(data.Length - 1, 1));
+      int time = int.Parse(data.Substring(0, data.Length - 1));
+
+      // System.Console.WriteLine(data);
+      System.Console.WriteLine(type);
+      System.Console.WriteLine(time);
+    }
+
+    static void Start(int time)
+    {
+      int currentTime = 0;
+
+      while (currentTime != time)
+      {
+        Console.Clear();
+        currentTime++;
+        System.Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+      }
+      Console.Clear();
+      System.Console.WriteLine("Stopwatch Finished!");
+      Thread.Sleep(2500);
+    }
+  }
+}
